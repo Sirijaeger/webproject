ff5f1523fb8fc90784f48ff8f827facd7006f45f
sirijaeger <57160656@go.buu.ac.th>
2017-12-17 20:20:13 +0700

    datacontract

---
+++ contacts/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express.Router()
+
+let DataContacts = [
+    {ID: 0, Name: 'Sirichai Songwas', Email: '57160656@go.buu.ac.th', Phone: '0925049334'},
+    {ID: 1, Name: 'Pimnapa Klahan', Email: '57160014@go.buu.ac.th', Phone: '098-7765432'},
+    {ID: 2, Name: 'Anutida Puengrung', Email: '57160361@go.buu.ac.th', Phone: '084-0997967'},
+    {ID: 3, Name: 'Sakuntra Chaladha', Email: '57160006@go.buu.ac.th', Phone: '087-6675143'},
+  ];
+
+app.get('/contacts', (req, res) => {
+    res.json(DataContacts)
+})
+
+module.exports = app
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const contactsRouter = require('../contacts')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', contactsRouter)
+
+module.exports = app
